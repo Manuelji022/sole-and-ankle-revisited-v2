d89c786ec333e22233e9515da3364c58f6c20c15
Joshua Comeau <joshwcomeau@gmail.com>
2026-07-23 10:49:23 -0400

    Remove potentially-misleading comment about closing dialogs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,11 +9,6 @@ import MobileMenu from '../MobileMenu';
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
-
   return (
     <header>
       <SuperHeader />
